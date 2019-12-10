eca9ad1b9246d7f633549121124c63121344fbdd
Sergo Mikirtumov <mikirtumov@gmail.com>
2019-12-09 21:04:23 -0500

    updated calendar css and background

--- public/js/index.js
+++ public/js/index.js
@@ -3,7 +3,24 @@ var $exampleText = $("#example-text");
 var $exampleDescription = $("#example-description");
 var $submitBtn = $("#submit");
 var $exampleList = $("#example-list");
-
+var cards = [{
+  "greeting":"<div id='greetingBlock'><p>Dear <span id='childOne'></span><span id='childTwo'></span></div>",
+  "message":"<div id='messageBlock'><p>When Santa told us we would be your elves for the festival of the elves we were Sooooooooo EXCITED. Our names are <span id='childOne'> and </span><span id='childTwo'></span>. We will bring you Christmas cheer and fun this year. We left you dolls that look like us, so first thing to do is give us a hug.</p>
+  <p>Festival of the Elves is a magicaly time. Its about having fun with your family and for being kind.</p>
+  <p>Each day you will find a special note left behind.Today is our first and we are just about to burst. </p>
+  <p>Before we say goodbye there are three things to try.
+    1. Please make us a drawing for our workshop. We'll pick it up when we leave your note tomorrow.
+    2. Spin around three times.
+    3. Give someone you love a hug.
+  </p></div>",
+  "countdown":"<div id='countDownBlock'><span id='daysUntil'></span> days until Christmas Day!!!!</div>",
+  "close":"<div id='elfBlock'> Love,<span id='elfOne'> and </span><span id='elfTwo'></span> </div>"
+},{
+  "greeting":"",
+  "message":"",
+  "countdown":"",
+  "close":""
+}];
 // The API object contains methods for each kind of request we'll make
 var API = {
   saveExample: function(example) {
