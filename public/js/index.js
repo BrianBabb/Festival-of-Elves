81a0fd700cbfb174e2d43d8d52b8c7bbc26f7e8a
Sugeeta32 <sugeeta.gupta@gmail.com>
2019-12-09 20:31:17 -0500

    deleted Sugeeta's calendar

--- public/js/index.js
+++ public/js/index.js
@@ -1,43 +1,5 @@
 // Get references to page elements
 
-var app = {
-  settings: {
-    container: $(".calendar"),
-    calendar: $(".front"),
-    days: $(".weeks span"),
-    form: $(".back"),
-    input: $(".back input"),
-    buttons: $(".back button")
-  },
-
-  init: function() {
-    instance = this;
-    settings = this.settings;
-    this.bindUIActions();
-  },
-
-  swap: function(currentSide, desiredSide) {
-    settings.container.toggleClass("flip");
-
-    currentSide.fadeOut(900);
-    currentSide.hide();
-
-    desiredSide.show();
-  },
-
-  bindUIActions: function() {
-    settings.days.on("click", function() {
-      instance.swap(settings.calendar, settings.form);
-      settings.input.focus();
-    });
-
-    settings.buttons.on("click", function() {
-      instance.swap(settings.form, settings.calendar);
-    });
-  }
-};
-
-app.init();
 // var $exampleText = $("#example-text");
 // var $exampleDescription = $("#example-description");
 // var $submitBtn = $("#submit");
